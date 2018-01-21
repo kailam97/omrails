7c87dcba24f1482a5c04e0c8e0088728a81f3612
Kai Lam <prelovedny@gmail.copm>
2018-01-21 16:08:11 -0500

    Added usernames and names to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,6 @@ class User < ApplicationRecord
 
   has_many :tweets
   has_many :items
+  validates :username, presence: true, uniqueness: true, length: {minimum: 6}
+  validates :name, presence: true
 end
